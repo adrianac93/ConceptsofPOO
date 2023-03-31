0b959d8836183c044b6c1825527ea51bec6c50ef
adrianac93 <129550023+adrianac93@users.noreply.github.com>
2023-03-31 16:44:45 -0400

    Add files via upload

---
+++ ConceptsPOO 3.0/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using ConceptsPOO_3._0;
+
+Console.WriteLine("POO Concepts 3.0");
+Console.WriteLine("================");
+
+try
+{
+	Console.WriteLine(new Date3(2024, 2, 29));
+	Console.WriteLine(new Date3(2009, 9, 23));
+	Console.WriteLine(new Date3(2008, 11, 30));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
