b91706e04369463d48e6d39341dae5a2a39cb0a2
adriana maestre <adrianalamasbonita_15@hotmail.com>
2023-04-12 20:59:29 -0400

    Finish date class

---
+++ POOConcepts4/POOConcepts4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using POOConcepts4;
+
+Console.WriteLine("POOConcepts4");
+Console.WriteLine("============");
+
+try
+{
+	Console.WriteLine(new Date4(2024, 2, 29));
+	Console.WriteLine(new Date4(2009, 9, 23));
+	Console.WriteLine(new Date4(2008, 11, 30));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
